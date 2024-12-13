a1ffce4818e6441609447a4913f4cbe29b1af405
Jorge C. Pereira <64277389+jp2435@users.noreply.github.com>
2024-12-13 13:03:55 +0000

    Teste modelo 2, ex02

---
+++ testes/modelo_2/ex02.asm
@@ -0,0 +1,50 @@
+    .data
+constantes: .float -1.0,0.0,1.0
+    .text
+    .globl func2
+
+func2:
+    # $a0 -> $t0 -> Pointer de Float
+    # $f12 -> $f2 -> Float(t)
+    # $a1 -> $t1 -> Int(n)
+    
+    # Registos
+    # a -> $t0
+    # t ->  $f2
+    # n -> $t2
+    # oldg -> $f4
+    # g -> $f6
+    # s -> $f8
+    # k: $t3
+
+    la $t4,constantes
+    l.s $f4,0($t4) # oldg = -1.0
+    l.s $f6,8($t4) # g = 1.0
+    l.s $f8,4($t4) # s = 0.0
+
+    li $t3,0 # k=0
+for:
+    bge $t3,$t2,end_for # if k>=n j end_for
+while:
+    sub.s $f10,$f6,$f4 # g-oldg
+    c.le.s $f10,$f2 
+    bc1t end_while  # if (g-oldg)<=t j end_while
+    mov.s $f4,$f6 # oldg=g
+    sll $t5,$t3,2 # k*4
+    addu $t5,$t0,$t5 # &a[k]
+    l.s $f10,0($t5) # a[k]
+    add.s $f10,$f6,$f10 # g+a[k]
+    div.s $f6,$f10,$f2 # g=(g+a[k])/t
+    j while
+end_while:
+    add.s $f8,$f8,$f6 # s+=g
+    s.s $f6,0($t5) # $t5->&a[k], a[k]=g
+
+    addiu $t3,$t3,1 # k++
+    j for
+end_for:
+    mtc1 $t2,$f10
+    cvt.s.w $f10,$f10 # Casting Float
+    div.s $f0,$f8,$f10 # return s/(float)n
+    jr $ra
+
